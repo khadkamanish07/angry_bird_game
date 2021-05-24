c066d7b197d48e62fce94b1f8b67af34a85eb20f
khadkamanish07 <61181892+khadkamanish07@users.noreply.github.com>
2021-05-23 19:57:11 -0500

    Add files via upload

---
+++ Assets/LevelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex =1 ;
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Enemy enem in _enemies)
+        {
+            if (enem != null)
+                return;
+        }
+
+        Debug.Log("Emenies Dead");
+
+        
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
